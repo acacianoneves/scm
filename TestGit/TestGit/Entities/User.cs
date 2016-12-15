790eeea15821a21e8401a2b1d9b32cfa42a1f61c
Acaciano Neves Souza <acaciano.neves@hotmail.com>
2016-12-15 16:36:39 -0200

    teste 5

--- TestGit/TestGit/Entities/User.cs
+++ TestGit/TestGit/Entities/User.cs
@@ -4,5 +4,7 @@ namespace TestGit.Entities
     public class User
     {
         public string Name { get; set; }
+        public string Email { get; set; }
+        public string Telefone { get; set; }
     }
 }
