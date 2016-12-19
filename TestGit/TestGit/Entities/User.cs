a375aa1de740979e1343a74304dd8f66e4ffaac8
Acaciano Neves Souza <acaciano.neves@hotmail.com>
2016-12-19 10:16:04 -0200

    Novas entidades

--- TestGit/TestGit/Entities/User.cs
+++ TestGit/TestGit/Entities/User.cs
@@ -3,6 +3,8 @@ namespace TestGit.Entities
 {
     public class User
     {
+        public int Id { get; set; }
+        public int IdUser { get; set; }
         public string Name { get; set; }
         public string Email { get; set; }
         public string Telefone { get; set; }
---
+++ TestGit/TestGit/Entities/UserCode.cs
@@ -0,0 +1,12 @@
+﻿
+using System;
+
+namespace TestGit.Entities
+{
+    public class UserCode
+    {
+        public int Id { get; set; }
+        public int IdUser { get; set; }
+        public Guid Code { get; set; }
+    }
+}
